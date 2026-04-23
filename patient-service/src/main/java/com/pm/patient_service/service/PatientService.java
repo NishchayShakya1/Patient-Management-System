e34eae46f7d90816a56c66873712ccfd241da208
Nishchay Shakya <shakyanischay420@gmail.com>
2026-04-23 16:45:06 +0530

    PatientController, PatientService, PatientRepo, Exception and DTO Implemented

---
+++ patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -0,0 +1,36 @@
+package com.pm.patient_service.service;
+
+import com.pm.patient_service.dto.PatientRequestDTO;
+import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.exception.EmailAlreadyExistsException;
+import com.pm.patient_service.mapper.PatientMapper;
+import com.pm.patient_service.model.Patient;
+import com.pm.patient_service.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+    private PatientRepository patientRepository;
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients(){
+        List<Patient> patients = patientRepository.findAll();
+
+        // Iterate through each 'patients' entity and map it to 'DTO' entity.
+        return patients.stream().map(PatientMapper::toDTO).toList();
+    }
+
+    public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO){
+
+        if(patientRepository.existsByEmail(patientRequestDTO.getEmail())){
+            throw new EmailAlreadyExistsException("A patient with this email " + "already exists " + patientRequestDTO.getEmail());
+        }
+
+        Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
+        return PatientMapper.toDTO(newPatient);
+    }
+}
