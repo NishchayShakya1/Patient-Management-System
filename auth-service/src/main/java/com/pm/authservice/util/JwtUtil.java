f7ae3ed3f9b81954c061379e0cb63b21b9fcd7df
Nishchay Shakya <shakyanischay420@gmail.com>
2026-05-10 17:16:23 +0530

    Jwt Validation Gateway Filter added

--- auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
+++ auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
@@ -1,16 +1,21 @@
 package com.pm.authservice.util;
 
 import com.pm.authservice.dto.LoginRequestDTO;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
 
+@Component
 public class JwtUtil {
     private final Key secretKey;
 
@@ -27,5 +32,17 @@ public class JwtUtil {
                 .expiration(new Date(System.currentTimeMillis()+1000 *60*60*10))
                 .signWith(secretKey)
                 .compact();
+    }
+
+    public void validateToken(String token) {
+        try {
+            Jwts.parser().verifyWith((SecretKey) secretKey).build().parseSignedClaims(token);
+        }
+        catch (SignatureException e){
+            throw new JwtException("Invalid JWT Signature");
+        }
+        catch (JwtException e){
+            throw new JwtException("Invalid JWT token");
+        }
     }
 }
